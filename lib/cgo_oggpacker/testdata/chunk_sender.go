70b66a1b3621b4e37381e85cc2c2c695f1d82fec
Pavel Druzhinin <paveldroo@gmail.com>
2024-08-20 01:19:46 +0300

    Add sampleCount for ogg file + alaw_raw file for testing

--- lib/cgo_oggpacker/testdata/chunk_sender.go
+++ lib/cgo_oggpacker/testdata/chunk_sender.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	"os"
 	"path"
+
+	"github.com/paveldroo/go-ogg-packer/lib"
 )
 
+const sampleRate = 8000
+
 func AudioByChunks() [][]byte {
-	var chunkSize = 512
+	var chunkSize = lib.SamplesCnt(sampleRate)
 	d := RefOGGData()
 
 	var res [][]byte
@@ -23,30 +27,13 @@ func AudioByChunks() [][]byte {
 	return res
 }
 
-// NewRefAudioChunkSender sends reference audio file in channel chunk by chunk
-//func NewRefAudioChunkSender() <-chan []byte {
-//	const chunkSize = 512
-//	d := RefOGGData()
-//	ch := make(chan []byte)
-//	go func() {
-//		for i := 0; i < len(d); i += chunkSize {
-//			end := i+chunkSize
-//			if end > len(d) {
-//				end = len(d) - 1
-//			}
-//			ch <- d[i:end]
-//		}
-//	}()
-//
-//	return ch
-//}
-
 func RefOGGData() []byte {
+	const testFilePath = "testdata/audio/office.alaw_raw"
 	wDir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("get current work directory: %s", err.Error())
 	}
-	var fPath = path.Join(wDir, "testdata/audio/ref/office.ogg")
+	var fPath = path.Join(wDir, testFilePath)
 
 	d, err := os.ReadFile(fPath)
 	if err != nil {
